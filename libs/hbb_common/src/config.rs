95cf9ffdb117f1011ebd8695e773f5a773ebb296
806611754 <806611754@qq.com>
2024-10-21 08:47:15 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["drivce.letcon.com.cn"];
-pub const PUBLIC_RS_PUB_KEY: &str = "yLRm08drh+dj4b82YYadQaGaccBtD8e3q0QnDNrLpyY=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["domain.com.cn"];
+pub const PUBLIC_RS_PUB_KEY: &str = "yLRm08DDdrh+dj4b8AA2sYYadQaGacAAcBtDd8e3q0QBBnDNrLpyY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
