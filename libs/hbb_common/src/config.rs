76146f7d41eeb4dd6733528a3fc7bfe18797ee20
806611754 <806611754@qq.com>
2024-11-01 08:43:08 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["119.145.41.222","172.21.110.5","drivce.letcon.com.cn"];
+pub const PUBLIC_RS_PUB_KEY: &str = "yLRm08drh+dj4b82YYadQaGaccBtD8e3q0QnDNrLpyY=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
